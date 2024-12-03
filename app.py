94dcf8d3c26218af4806cba592f2595150ff2c6c
RikuDaiSoraFuu <131649704+RikuDaiSoraFuu@users.noreply.github.com>
2024-12-03 21:24:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -57,4 +57,4 @@ def encrypt_file():
     return send_file(encrypted_file_path, as_attachment=True, download_name=file.filename + '.enc')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
