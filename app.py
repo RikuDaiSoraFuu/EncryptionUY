b1fee7d41fbd11236ae8ffb4282b3448c02939f7
RikuDaiSoraFuu <131649704+RikuDaiSoraFuu@users.noreply.github.com>
2024-12-05 16:04:17 +0900

    Update app.py

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def encrypt_file():
     )
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)),debug=True)
